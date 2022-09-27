a0b56a697ec6280ed95eb13376b686c5372be0bc
ferbravo00 <fer.bp001@gmail.com>
2022-09-27 10:20:35 +0200

    cas

--- TiposPrim/src/tiposprim/TiposPrim.java
+++ TiposPrim/src/tiposprim/TiposPrim.java
@@ -16,6 +16,71 @@ public class TiposPrim {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("");
+        System.out.println("\t ENTEROS");
+        System.out.println("\t-------");
+        System.out.println("");
+        
+        System.out.println("BYTE");
+        System.out.println("----");
+        System.out.println("Valor minimo es: "+Byte.MIN_VALUE);
+        System.out.println("Valor minimo es: "+Byte.MAX_VALUE);
+        
+        System.out.println("");
+        
+        System.out.println("SHORT");
+        System.out.println("-----");
+        System.out.println("Valor minimo es: "+Short.MIN_VALUE);
+        System.out.println("Valor minimo es: "+Short.MAX_VALUE);
+        
+        System.out.println("");
+        
+        System.out.println("INT");
+        System.out.println("---");
+        System.out.println("Valor minimo es: "+Integer.MIN_VALUE);
+        System.out.println("Valor minimo es: "+Integer.MAX_VALUE);
+        
+        System.out.println("");
+        
+        System.out.println("LONG");
+        System.out.println("----");
+        System.out.println("Valor minimo es: "+Long.MIN_VALUE);
+        System.out.println("Valor minimo es: "+Long.MAX_VALUE);
+        
+        System.out.println("");
+
+        
+        System.out.println("");
+        System.out.println("\t DECIMALES");
+        System.out.println("\t-------");
+        System.out.println("");
+        
+        System.out.println("FLOAT");
+        System.out.println("-----");
+        System.out.println("Valor minimo es: "+Float.MIN_VALUE);
+        System.out.println("Valor minimo es: "+Float.MAX_VALUE);
+        
+        System.out.println("");
+        
+        System.out.println("DOUBLE");
+        System.out.println("------");
+        System.out.println("Valor minimo es: "+Double.MIN_VALUE);
+        System.out.println("Valor minimo es: "+Double.MAX_VALUE);
+        
+        System.out.println("");
+        
+        System.out.println("");
+        System.out.println("\t CARACTER");
+        System.out.println("\t-------");
+        System.out.println("");
+        
+        System.out.println("");
+        
+        System.out.println("CHAR");
+        System.out.println("----");
+        //System.out.println("Valor minimo es: "+Schar.MIN_VALUE);
+        //System.out.println("Valor minimo es: "+Double.MAX_VALUE);
     }
     
 }
